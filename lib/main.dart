a7f81d90fe7711c901dab72e40e4411b789c3593
Akshil <shahakshil2001@gmail.com>
2023-10-25 11:05:52 -0700

    ios added on firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
+import 'package:auth_app/firebase_options.dart';
 import 'package:auth_app/screens/home_screen.dart';
 import 'package:auth_app/screens/signin_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// import 'package:device_preview/device_preview.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     // DevicePreview(
-    // enabled: !kReleaseMode,
-    // builder: (context) =>
+    //   enabled: !kReleaseMode,
+    //   builder: (context) =>
     MyApp(), // For displaying app in google chrome
     // ),
   );
